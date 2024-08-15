4e08474b6ec3111cd8cf19e977235441b7b80bc3
xmax1989 <xmaxboev@yandex.ru>
2024-08-15 11:59:14 +0300

    problems

---
+++ lesson_6.1/auth_phorm.py
@@ -0,0 +1,6 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service as ChromeService
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.by import By
+
+driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
